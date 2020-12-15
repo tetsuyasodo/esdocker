79d827742abe032634dd85fe318a29f2f4fd9bc2
Tetsuya Sodo <tetsuya.sodo@gmail.com>
2020-12-15 11:20:07 +0900

    Create essetup-single.sh

---
+++ essingle/essetup-single.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+cat <<'EOF' >/etc/yum.repos.d/es.repo
+[elasticsearch-7.x]
+name=Elasticsearch repository for 7.x packages
+baseurl=https://artifacts.elastic.co/packages/7.x/yum
+gpgcheck=1
+gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
+enabled=1
+autorefresh=1
+type=rpm-md
+EOF
+
+yum install -y elasticsearch kibana
+
+cat <<'EOF' >>/etc/elasticsearch/elasticsearch.yml
+cluster.name: cluster01
+network.host: 0.0.0.0
+discovery.seed_hosts: ["es01"]
+cluster.initial_master_nodes:  ["es01"]
+EOF
+
+cat <<'EOF' >>/etc/kibana/kibana.yml
+server.host: "0.0.0.0"
+EOF
+
+systemctl daemon-reload
+systemctl start elasticsearch
+systemctl start kibana
